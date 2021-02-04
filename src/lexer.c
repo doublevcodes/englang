96cf60f6a8c08f41c5df8bde2c23c3c740f11c3b
Vivaan Verma <vivaan.verma@gmail.com>
2021-02-04 15:47:07 +0000

    Its in progress

--- src/lexer.c
+++ src/lexer.c
@@ -29,13 +29,31 @@ char *to_char_array(char filename[]) {
   }
   array_len = array_ptr;
 
-cleanup:
-  return array;
+    cleanup:
+        return array;
+}
+
+char *scan_string(char *ptr, int i){
+    int j = 0;
+    char cur = ptr[i];
+    char ret[32] = {};
+    while(!strcmp(cur, "'")) {
+        ret[j] = cur;
+        i++;
+        j++;
+    }
+    return ret;
 }
 
 void lex(char *ptr) {
     for(int i=0; i<array_len; i++){
-   		  putchar(ptr[i]);
+   		char cur = ptr[i];
+        if (cur == '\n') {
+            printf("\\n : NEWLINE\n");
+        } else if (strcmp(cur, "'")) {
+            char string[] = scan_string(ptr, i);
+            printf(string);
+        }
 	}
 }
 
